f33f00938823d6abe1f3efb0c037c7fddcf7d3c9
Adrien Haxaire <adrien.haxaire@lunatech.com>
2018-12-11 20:51:18 +0100

    Put the parser in its own module

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,9 +1,14 @@
+extern crate curie;
+use curie::parser;
+
 use std::io::{self, BufRead};
 
 fn main() {
+    assert_eq!(parser::hex_color("#2F14DF"), Ok(("", parser::Color {red: 47, green: 20, blue: 223, })));
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        let result = line.unwrap();
-        println!("{}", result);
+        let input_line = line.unwrap();
+        let result = parser::hex_color(&input_line).unwrap().1;
+        println!("{:?}", result);
     }
 }
