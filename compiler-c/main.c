5acdb18eb927052a80d8b435faa083faca939138
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2019-01-09 10:23:14 +0100

    Refactored the parse function to take expressions

--- compiler-c/main.c
+++ compiler-c/main.c
@@ -40,96 +40,56 @@ cell* append(char token, cell* c)
   }
 }
 
-void parse()
+cell* parse(const char* expr)
 {
   char token;
   bool done = false;
 
-  char buf[1000] = "(* (+ 1 2) 4)";
   int i = 0;
   int nLeftParen = 0;
   int nRightParen = 0;
 
+  cell* list = NULL;
+  
   while (!done) {
-    token = buf[i];;
+    token = expr[i];;
     switch(token) {
     case '(':
+      list = append('(', list);
       nLeftParen++;
       i++;
       break;
     case ')':
+      list = append(')', list);
       nRightParen++;
       i++;
       if (nLeftParen == nRightParen)
         done = true;
       break;
-    case ' ':
-      i++;
-      break;
     default:
+      list = append(token, list);
       i++;
       break;
     }
   }
-}
 
-typedef struct char_token {
-	char c;
-	struct char_token* next;
-} char_token;
-
-char_token* init() {
-	char_token* token = malloc(sizeof(char_token));
-	token->next = NULL;
-	return token;
-}
-
-char_token* charToken(char c) {
-	char_token* res = init();
-	res->c = c;
-	return res;
-}
-
-int izmars_main(void)
-{
-  const char* code = "(lambda () \"Hello World\")";
-
-  char_token *head = charToken(code[0]);
-  char_token *prev = head;
-
-  const size_t length = strlen(code);
-  for (int i = 1; i < length; i++) {
-    char_token *curr = charToken(code[i]);
-    prev->next = curr;
-    prev = curr;
-  }
-
-  char_token* curr = head;
-
-  while (curr != NULL) {
-    printf("%c", curr->c);
-    curr = curr->next;
-  }
-
-  return 0;
+  return list;
 }
 
 int main(void)
 {
-  // const char expression = "(* (+ 1 2) 4)";
+  const char *expression = "(* (+ 1 2) 4)";
 
-  cell *head = new_cell('(');
-  cell *list = head;
+  cell *list = NULL;
+  cell *cur = NULL;
 
-  list = append('*', list);
-  list = append(')', list);
+  list = parse(expression);
 
-  cell* cur = list;
+  cur = list;
   while (cur != NULL) {
     printf("%c", cur->car);
     cur = cur->cdr;
   }
 
-  // izmars_main();
-
+  return EXIT_SUCCESS;
 }
