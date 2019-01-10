aed96d888128d53635d0cf8dd7655b23e2c604e4
Adrien Haxaire <adrien.haxaire@lunatech.com>
2019-01-10 13:51:47 +0100

    Deleted the compiler-clojure directory

--- compiler-clojure/test/curie/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns compiler-clojure.core-test
-  (:require [clojure.test :refer :all]
-            [compiler-clojure.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
